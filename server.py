75b4e3e26b183430910fe7755cf7ae6661b7cafa
Joshua Varga <jpewpoopachew@gmail.com>
2022-05-09 15:32:19 -0400

    Fixed webserver

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
 from os import environ
 from flask import Flask
 
-address = environ.get('ADDRESS')
-port = environ.get('PORT')
-
 app = Flask(__name__)
-app.run(address=address, port=port)
+app.run(host=environ.get('ADDRESS'), port=environ.get('PORT'))
