de1ffab58c3722bdd1c405e8d7a37b6219760391
Joshua Varga <jpewpoopachew@gmail.com>
2022-07-13 19:16:37 -0400

    archived threads retreived

--- utils/find_thread.py
+++ utils/find_thread.py
@@ -1,9 +1,17 @@
 from discord import TextChannel, Thread
 
-def find_thread(name: str, channel: TextChannel) -> Thread:
+async def find_thread(name: str, channel: TextChannel) -> Thread:
     nameFormatted = name.lower().replace(' ','')
 
+    archived_thread = channel.archived_threads()
+
     foundThread = None
+
+    async for thread in archived_thread:
+        threadName = thread.name.lower().replace(' ', '')
+
+        if threadName == nameFormatted:
+            foundThread = thread
 
     for thread in channel.threads:
         threadName = thread.name.lower().replace(' ', '')
